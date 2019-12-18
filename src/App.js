345c8246a8e63fc3dc55925328a870561ff89272
Michelle Vu <michelle.vu@aviture.us.com>
2019-12-18 17:10:33 -0600

    times added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      score: 0,
+      points: 1,
+      cost: 20,
+      timeStart: new Date().toLocaleTimeString(),
+      timeCurrent: 0,
+      timeElapsed: 0,
+      elapsedStart: 0
+    }
+  }
+
+  componentDidMount(){
+    this.currTimeInterval = setInterval(() => this.currTime(), 1000);
+    this.timeElapsedCounter = setInterval(() => this.elapsedTime(), 1000)
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.currTimeInterval);
+    clearInterval(this.timeElapsedCounter)
+  }
+
+  handleClick(){
+    let points = this.state.points;
+    let score = this.state.score;
+    
+    let newScore = score + points;
+    this.setState({
+      score: newScore
+    })
+    
+  }
+
+  powerUp(){
+    let cost = this.state.cost;
+    let currScore = this.state.score - cost;
+
+    if((currScore) < 0 ){
+      return alert('Not enough points')
+    }
+
+    let points = this.state.points + 1;
+    let newCost = cost + 20;
+    this.setState({
+      points: points,
+      score: currScore,
+      cost: newCost
+    })
+  }
+
+  currTime(){
+    this.setState({
+      timeCurrent: new Date().toLocaleTimeString()
+    })
+  }
+
+  elapsedTime(){
+    let addSecond = this.state.timeElapsed + 1;
+
+    this.setState({
+      timeElapsed: addSecond
+    })
+
+  }
+
+
+  render() {
+    const {score, cost, points} = this.state;
+    const {timeStart, timeCurrent, timeElapsed} = this.state;
+
+    return (
+      <div className='App-header'>
+        <div className='App'>
+          <div>Start time: {timeStart}</div>
+          <div>Current time: {timeCurrent}</div>
+          <div>Time elapsed: {timeElapsed} secs</div>
+        </div>
+        <div className='score'>{score}</div>
+          
+        <div>
+          <button onClick={() => this.handleClick()}>Add {points}</button>
+          <button onClick={() => this.powerUp()}>Power Up ({cost})</button>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,19 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/Start Time/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('the score is zero', () => {
+  const { container } = render(< App/>)
+  const startScore = container.querySelector('.score').innerHTML;
+  expect(startScore).toBe('0');
+})
+
+
+//when click add button, it increments the score
+  //start game with 20 points, verify state
+  //make sure game logic is correct
